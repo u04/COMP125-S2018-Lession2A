2ae85ed57a409b53729b98ff8a48741d999af40d
cdey1 <300920267@cencol.ca>
2018-05-08 14:11:27 -0400

    added IIFE to app.js

---
+++ Scripts/app.js
@@ -0,0 +1,13 @@
+/* Custom JS goes here */
+
+// IIFE - Immedately Invoked Function Expression
+// annoymos self executing function
+(function(){
+
+    function Start() {
+        console.log("App Started!");
+    }
+
+    window.onload = Start;
+
+})();
